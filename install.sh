4dfc068661ecd00acce535c2f544f50f70541af7
Joscha <zippelman@web.de>
2025-02-02 12:21:13 +0100

    fix install.sh output

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "selected arch: ${ARCH}"
-
 ARCH=$(uname -m)
 if [ "${ARCH}" == "x86_64" ]; then
   ARCH="x86_64"
@@ -12,6 +10,7 @@ else
   exit 1
 fi
 
+echo "selected arch: ${ARCH}"
 RELEASE_META_DATA_URL="https://api.github.com/repos/sejoharp/worktimers/releases/latest"
 BINARY_URL=$(curl -s ${RELEASE_META_DATA_URL} | jq -r ".assets[] | select(.name | contains(\"${ARCH}\")) | .browser_download_url")
 echo "downloading ${BINARY_URL}"
